f66cd85b5150fe0d20d249518120873934ee0b3d
JamilyJorge25 <jamisjorge25@gmail.com>
2024-12-03 16:07:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'O que é moral?',
+    'é um conjunto de convenções sociais estabelecidas com o objetivo de alcançar a boa convivência social.'
+)
+
+criaCartao(
+    'Historia',
+    'o que e historia?',
+    'conjunto de conhecimentos relativos ao passado da humanidade e sua evolução, segundo o lugar, a época, o ponto de vista escolhido'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é energia limpa?',
+    'é aquela produzida a partir de fontes renováveis, disponíveis em maior ou menor abundância na natureza. '
+)
+
+criaCartao(
+    'Quimica',
+    'O que é a lei de lavosier?',
+    'Em um sistema fechado, a massa dos reagentes de uma reação química é igual à massa dos produtos. Toda a ideia da conservação de massas foi trazida por Lavoisier em seu livro Tratado elementar de Química. '
+)
